53f2e426eed2108e0fcd9b5a2f7c3ab6dd52d718
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-06-11 08:25:58 -0300

    Criando classe Voo

---
+++ src/com/company/Aeroporto/Voo.java
@@ -0,0 +1,9 @@
+package com.company.Aeroporto;
+
+public class Voo {
+  private int numero;
+  private String dataPartida;
+  private String horarioPartida;
+  private String horarioChegada;
+  private int vagas;
+}
