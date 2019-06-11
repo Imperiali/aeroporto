ac820624d548cb403dab875f5268b21349783f7d
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-06-11 08:41:06 -0300

    Atualizando classe Voo

--- src/com/company/Aeroporto/Voo.java
+++ src/com/company/Aeroporto/Voo.java
@@ -1,6 +1,9 @@
 package com.company.Aeroporto;
 
 public class Voo {
+  /*
+  TODO Um voo possui número, data de partida, horário de partida, horário de chegada e número de vagas. Além disso, um vôo está associado aos aeroportos de origem, de destino e a aeronave. Todos os voos de aviões ocorrem uma vez para cada dia da semana. O sistema deve permitir incluir, remover e editar voos. A partir do número de um voo, obter o número de vagas disponíveis, a data de partida, o horário de partida, o horário de chegada, a descrição da aeronave empregada, os códigos e os nomes dos aeroportos de origem e de destino. Implemente uma classe que contemple os atributos e métodos apresentados.
+  * */
   private int numero;
   private String dataPartida;
   private String horarioPartida;
