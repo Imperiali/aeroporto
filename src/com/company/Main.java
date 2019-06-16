c1ce4b077a96bdc363f633a09350bbeb9ba0a556
imperiali <igor.imperiali@gmail.com>
2019-06-16 19:18:09 -0300

    Preenchendo main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import com.company.Aeronave.Aeronave;
+import com.company.Aeronave.Aviao;
+import com.company.Aeronave.Helicoptero;
+import com.company.Aeroporto.Aeroporto;
+import com.company.Aeroporto.Cidade;
+import com.company.Aeroporto.Voo;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Aeronave comercial = new Aviao("US","TS18", "SpaceX", 30, "TAM" );
+        Aeronave helicopter = new Helicoptero("FX", "Chopper", "USSPlanes", "Resgate");
+        Aeronave jatinho = new Aviao("TTR","BBDS", "Aeromodelos Brasileiros", 50, "GOL" );
+
+        Cidade rioDeJaneiro = new Cidade("Rio de Janeiro", 235.0);
+        Cidade saoPaulo = new Cidade("São Paulo", 215.0);
+
+        Aeroporto congonhas = new Aeroporto("545", "Aeroporto internacional de Congonhas");
+        Aeroporto galeao = new Aeroporto("616", "Aeroporto internacional Tom Jobbin");
+
+        Voo rjsp = new Voo(555, "12/05/2019", "10:36", "12:48", 50, galeao, congonhas, comercial );
+        Voo sprj = new Voo(564, "15/05/2019", "11:76", "13:57", 40, congonhas, galeao, jatinho );
     }
 }
